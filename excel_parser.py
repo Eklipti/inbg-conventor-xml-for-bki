8a215d74ab18bcbca91eb14a8901821add31954c
Eklipti <zadydolimi@gmail.com>
2026-02-20 15:09:10 +0600

    feat: changes in data storage logic

--- excel_parser.py
+++ excel_parser.py
@@ -5,15 +5,17 @@ from pathlib import Path
 def parse_active_sheet(file_path: Path, logger: logging.Logger) -> dict:
     logger.debug(f"Начинаем парсинг данных из файла: {file_path}")
     
-    wb = openpyxl.load_workbook(file_path, data_only=True) # читает значения
+    wb = openpyxl.load_workbook(file_path, data_only=True)
     sheet = wb["Активные"]
     
-    parsed_data = {0: "null"} # нулевой элемент для синхронизации
+    parsed_data = {0: "null"}
     
+    # Собираем все заголовки и их индексы колонок
     headers = {}
     for cell in sheet[2]:
-        if cell.value: # strip() убирает случайные пробелы по краям
+        if cell.value:
             headers[str(cell.value).strip()] = cell.column - 1 
+            
     col_v33 = headers.get("# в33")
     col_id = headers.get("ИД договора")
     
@@ -21,21 +23,25 @@ def parse_active_sheet(file_path: Path, logger: logging.Logger) -> dict:
         logger.critical("Критическая ошибка: на второй строке не найдены столбец 'ИД договора'.")
         return parsed_data
         
-    logger.info(f"Заголовки найдены. Начинаем чтение данных со строки 3...")
+    logger.debug(f"Заголовки найдены. Начинаем чтение данных со строки 3...")
     
     for row in sheet.iter_rows(min_row=3, values_only=True):
         v33_val = row[col_v33]
-        id_val = row[col_id]
         
-        # Если порядковый номер пуст, считаем, что таблица закончилась
         if v33_val is None:
             continue
             
         try:
             key = int(v33_val)
-            value = str(id_val) if id_val is not None else ""
             
-            parsed_data[key] = value
+            # --- ИЗМЕНЕНИЯ ЗДЕСЬ ---
+            # Теперь значением выступает вложенный словарь
+            parsed_data[key] = {
+                "ИД договора": str(row[col_id]) if row[col_id] is not None else "",
+                # Место для будущих полей:
+                # "Новое поле": row[headers.get("Название колонки")],
+            }
+            
         except ValueError:
             logger.warning(f"Не удалось преобразовать '# в33' в число: {v33_val}. Строка пропущена.")
             
